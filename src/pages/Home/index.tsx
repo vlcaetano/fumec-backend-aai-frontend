c74bb024a1088960d92a3abcbb79905525919090
Vitor Caetano <vitorlc95@gmail.com>
2021-06-11 19:56:13 -0300

    navigation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,17 @@
 import DataTable from "components/DataTable";
-import RegistrationForm from "components/RegistrationForm";
+
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <>
-    <h1>Home</h1>
-    <div className="container">
-      <h1 className="text-primary">Hello</h1>
-      <DataTable />
-      <RegistrationForm />
-    </div>
+      <h1>Home</h1>
+      <div className="container">
+        <h1 className="text-primary">Hello</h1>
+        <DataTable />
+        
+        <Link className="btn btn-primary" to="/cadastrar" > Cadastrar </Link>
+      </div>
     </>
   );
 }
--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,7 +1,14 @@
+import RegistrationForm from "components/RegistrationForm";
 
 function Registration() {
   return (
-    <h1>Registration</h1>
+    <>
+      <h1>Registration</h1>
+
+      <div className="container">
+        <RegistrationForm />
+      </div>
+    </>
   );
 }
 
