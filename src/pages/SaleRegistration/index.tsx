2a89ebc6b206c9dfcdf855070f1f45b6d6f303df
Vitor Caetano <vitorlc95@gmail.com>
2021-06-12 09:49:06 -0300

    Validation of Sale in SaleRegistration

--- src/pages/SaleRegistration/index.tsx
+++ src/pages/SaleRegistration/index.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 import { useHistory } from 'react-router-dom'
 
@@ -13,6 +13,11 @@ interface SaleData {
   }
 }
 
+interface ValidationError {
+  type: string
+  errorMsg: string
+}
+
 function Registration() {
   
   const history = useHistory()
@@ -20,6 +25,17 @@ function Registration() {
   const [amount, setAmount] = useState<number>(0)
   const [sellerId, setSellerId] = useState<number>(0)
   const [customerId, setCustomerId] = useState<number>(0)
+
+  const [errorList, setErrorList] = useState<ValidationError[]>([])
+  const [sellerError, setSellerError] = useState<ValidationError>({type: 'seller', errorMsg: ''})
+  const [customerError, setCustomerError] = useState<ValidationError>({type: 'customer', errorMsg: ''})
+
+  let customerExists: boolean = false
+  let sellerExists: boolean = false
+
+  useEffect(() => {
+      setErrorList([sellerError, customerError])
+  }, [sellerError, customerError])
 
   const handleAmountChange = (e: ChangeEvent<HTMLInputElement>) => {
     setAmount(parseFloat(e.target.value))
@@ -33,9 +49,39 @@ function Registration() {
     setCustomerId(parseFloat(e.target.value))
   }
 
+  const validadeCustomerId = async () => {
+    await axios.get(`${BASE_URL}/customers/${customerId}`)
+      .then(() => { 
+        customerExists = true 
+        setCustomerError({type: 'customer', errorMsg: ''})
+      })
+      .catch(() => {
+        customerExists = false
+        setCustomerError({type: 'customer', errorMsg: 'ID do cliente não encontrado'})
+      })
+  }
+
+  const validadeSellerId = async () => {
+    await axios.get(`${BASE_URL}/sellers/${sellerId}`)
+      .then(() => {
+        sellerExists = true
+        setSellerError({type: 'seller', errorMsg: ''})
+      })
+      .catch(() => {
+        sellerExists = false
+        setSellerError({type: 'seller', errorMsg: 'ID do vendedor não encontrado'})
+      })
+  }
 
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
+
+    await validadeSellerId()
+    await validadeCustomerId()
+
+    if (!sellerExists || !customerExists) {
+      return
+    }
 
     let saleData: SaleData = { 
         amount: amount,
@@ -58,17 +104,18 @@ function Registration() {
         <form onSubmit={(e) => handleSubmit(e)}>
         <div className="mb-3">
           <label className="form-label">Valor</label>
-          <input type="number" className="form-control" id="amount" onChange={e => handleAmountChange(e)} />
+          <input type="number" className="form-control" id="amount" onChange={e => handleAmountChange(e)} required />
         </div>
         <div className="mb-3">
           <label className="form-label">Id vendedor</label>
-          <input type="number" className="form-control" id="idSeller" onChange={e => handleCustomerIdChange(e)} />
+          <input type="number" className="form-control" id="idSeller" onChange={e => handleSellerIdChange(e)} required />
         </div>
         <div className="mb-3">
           <label className="form-label">Id cliente</label>
-          <input type="number" className="form-control" id="idCustomer" onChange={e => handleSellerIdChange(e)} />
+          <input type="number" className="form-control" id="idCustomer" onChange={e => handleCustomerIdChange(e)} required />
         </div>
         <button className="btn btn-primary" >Cadastrar</button>
+        {errorList.map((err) => <span className="mt-2 d-block text-danger" key={err.type}>{err.errorMsg}</span> )}
       </form>
       </div>
   );
