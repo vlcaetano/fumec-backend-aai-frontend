f0d8ee8e6a11c50d704e8c51819c2e4da3744c1b
Vitor Caetano <vitorlc95@gmail.com>
2021-06-12 10:25:49 -0300

    CPF Validation

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -1,7 +1,8 @@
 import axios from "axios";
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
 import { useHistory, useParams } from 'react-router-dom'
+import cpfValidation from "utils/cpfValidation";
 
 interface idParam {
   type: string
@@ -15,6 +16,13 @@ function Registration() {
   const [name, setName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
   const [cpf, setCpf] = useState<string>('')
+  
+  const [cpfError, setCpfError] = useState<string>('')
+  const [errorList, setErrorList] = useState<string>('')
+
+  useEffect(() => {
+    setErrorList(cpfError)
+  }, [cpfError])
 
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value)
@@ -31,6 +39,13 @@ function Registration() {
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault()
 
+    if (cpfValidation(cpf)) {
+      setCpfError('')
+    } else {
+      setCpfError('CPF inválido')
+      return
+    }
+
     let personData = {name, email, cpf}
     await axios.post(`${BASE_URL}/${type}`, personData)
 
@@ -38,27 +53,44 @@ function Registration() {
   }
 
   return (
-    <>
-      <h1>Cadastro</h1>
+    <div className="container">
+    <h1>Cadastro</h1>
 
-      <div className="container">
-        <form onSubmit={(e) => handleSubmit(e)}>
-        <div className="mb-3">
-          <label className="form-label">Nome</label>
-          <input type="text" className="form-control" id="name" onChange={e => handleNameChange(e)} />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" onChange={e => handleEmailChange(e)} />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">CPF</label>
-          <input type="text" className="form-control" id="cpf" onChange={e => handleCpfChange(e)} />
-        </div>
-        <button className="btn btn-primary" >Cadastrar</button>
-      </form>
+      <form onSubmit={(e) => handleSubmit(e)}>
+      <div className="mb-3">
+        <label className="form-label">Nome</label>
+        <input 
+          type="text" 
+          className="form-control" 
+          id="name" onChange={e => handleNameChange(e)} 
+          required 
+        />
       </div>
-    </>
+      <div className="mb-3">
+        <label className="form-label">Email</label>
+        <input 
+          type="email" 
+          className="form-control" 
+          id="email" 
+          onChange={e => handleEmailChange(e)} 
+          required 
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">CPF (Somente números)</label>
+        <input 
+          type="text" 
+          className="form-control" 
+          id="cpf" onChange={e => handleCpfChange(e)} 
+          minLength={11} 
+          maxLength={11} 
+          required
+        />
+      </div>
+      <button className="btn btn-primary" >Cadastrar</button>
+      <span className="mt-2 d-block text-danger">{errorList}</span>
+    </form>
+    </div>
   );
 }
 
